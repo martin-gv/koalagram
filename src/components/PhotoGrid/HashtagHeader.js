0f65ebc11ba8823c7892bcffdaf7d6587c116e8e
Martin Guardado <martinguardado@gmail.com>
2018-11-07 22:14:43 -0700

    Hashtag random photo stays same when component props change

--- src/components/PhotoGrid/HashtagHeader.js
+++ src/components/PhotoGrid/HashtagHeader.js
@@ -1,29 +1,37 @@
 import React from "react";
 
-const HashtagHeader = props => {
-  return (
-    <div>
-      <div className="row hashtag-header">
-        <div className="col-4">
-          <div
-            className="round-photo"
-            style={{
-              backgroundImage: "url('" + props.randomPhoto().image_url + "')"
-            }}
-          />
-        </div>
-        <div className="col-8">
-          <div className="hashtag">#{props.hashtag}</div>
-          <div className="stats">
-            <div className="photos">
-              <strong>{props.totalPhotos}</strong> photos
+class HashtagHeader extends React.Component {
+  state = { randomPhoto: "" };
+
+  componentDidMount() {
+    this.setState({ randomPhoto: this.props.randomPhoto().image_url });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="row hashtag-header">
+          <div className="col-4">
+            <div
+              className="round-photo"
+              style={{
+                backgroundImage: "url('" + this.state.randomPhoto + "')"
+              }}
+            />
+          </div>
+          <div className="col-8">
+            <div className="hashtag">#{this.props.hashtag}</div>
+            <div className="stats">
+              <div className="photos">
+                <strong>{this.props.totalPhotos}</strong> photos
+              </div>
             </div>
           </div>
         </div>
+        <hr />
       </div>
-      <hr />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default HashtagHeader;
